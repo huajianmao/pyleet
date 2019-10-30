c612b3fb63d12cc2d703b200c63e081cc3d6fe18
Huajian Mao <huajianmao@gmail.com>
2019-10-30 10:28:37 +0800

    Add solution for problem 56 merge intervals.

---
+++ solutions/a0056mergeintervals.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+################################################
+#
+# URL:
+# =====
+# https://leetcode.com/problems/merge-intervals/
+#
+# DESC:
+# =====
+# Given a collection of intervals, merge all overlapping intervals.
+#
+# Example 1:
+# Input: [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+# Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+#
+# Example 2:
+# Input: [[1,4],[4,5]]
+# Output: [[1,5]]
+# Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+#
+################################################
+from typing import List
+
+
+class Solution:
+  def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    if len(intervals) <= 1: return intervals
+    else:
+      result = []
+      intervals.sort(key=lambda x: x[0])
+      current = intervals[0]
+
+      for interval in intervals:
+        if interval[0] <= current[1]:
+          current = [current[0], max(interval[1], current[1])]
+        else:
+          result.append(current)
+          current = interval
+      result.append(current)
+      return result
---
+++ tests/test_a0056mergeintervals.py
@@ -0,0 +1,24 @@
+from solutions.a0056mergeintervals import Solution
+
+solution = Solution()
+
+
+def test_merge1():
+  intervals = [[1, 3], [8, 10], [2, 6], [15, 18]]
+  expect = [[1, 6], [8, 10], [15, 18]]
+  acutal = solution.merge(intervals)
+  assert acutal == expect
+
+
+def test_merge2():
+  intervals = [[4, 5], [1, 4]]
+  expect = [[1, 5]]
+  acutal = solution.merge(intervals)
+  assert acutal == expect
+
+
+def test_merge3():
+  intervals = [[1,4],[2,3]]
+  expect = [[1, 4]]
+  acutal = solution.merge(intervals)
+  assert acutal == expect
