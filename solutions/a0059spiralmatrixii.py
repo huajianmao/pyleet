798bc31db0ef23819e8dd89076601b9c4c7ae036
Huajian Mao <huajianmao@gmail.com>
2019-11-01 10:34:20 +0800

    Add solution for problem 59 spiral matrix ii

---
+++ solutions/a0059spiralmatrixii.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+################################################
+#
+# URL:
+# =====
+# https://leetcode.com/problems/spiral-matrix/
+#
+# DESC:
+# =====
+# Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+#
+# Example:
+#
+# Input: 3
+# Output:
+# [
+#  [ 1, 2, 3 ],
+#  [ 8, 9, 4 ],
+#  [ 7, 6, 5 ]
+# ]
+#
+#
+################################################
+from typing import List
+
+
+class Solution:
+
+  def generateMatrix(self, n: int) -> List[List[int]]:
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    num = 1
+    for round in range((n + 1) // 2 + 1):
+      for g in range(4):
+        ox, ix, signX = g // 2, g % 2, 1
+        if g >= 2: signX = -1
+
+        oy, iy, signY = 0, 1 - g % 2, 1
+        if g == 1 or g == 2: oy, signY = 1, -1
+
+        for i in range(n - 2 * round - 1):
+          x = signX * (round + ox * 1 + ix * i)
+          y = signY * (round + oy * 1 + iy * i)
+          matrix[x][y] = num
+          num += 1
+
+    if n % 2 == 1:
+      matrix[n//2][n//2] = num
+
+    return matrix
---
+++ tests/test_a0059spiralmatrixii.py
@@ -0,0 +1,22 @@
+from solutions.a0059spiralmatrixii import Solution
+
+solution = Solution()
+
+
+def test_generateMatrix1():
+  n = 3
+  expect = [
+    [1, 2, 3],
+    [8, 9, 4],
+    [7, 6, 5]]
+  actual = solution.generateMatrix(n)
+  assert actual == expect
+
+def test_generateMatrix2():
+  n = 2
+  expect = [
+    [1, 2],
+    [4, 3]
+  ]
+  actual = solution.generateMatrix(n)
+  assert actual == expect
