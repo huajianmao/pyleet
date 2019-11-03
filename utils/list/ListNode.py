aed30e5fefa295efb86938930a0d771494627846
Huajian Mao <huajianmao@gmail.com>
2019-11-03 09:11:28 +0800

    Add support methods for ListNode

--- utils/list/ListNode.py
+++ utils/list/ListNode.py
@@ -1,8 +1,36 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 
 # Definition for singly-linked list.
 class ListNode:
   def __init__(self, x):
     self.val = x
     self.next = None
+
+  @classmethod
+  def stringToListNode(cls, input):
+    # Generate list from the input
+    numbers = json.loads(input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+      ptr.next = ListNode(number)
+      ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+  @classmethod
+  def listNodeToString(cls, node):
+    if not node:
+      return "[]"
+
+    result = ""
+    while node:
+      result += str(node.val) + ", "
+      node = node.next
+    return "[" + result[:-2] + "]"
