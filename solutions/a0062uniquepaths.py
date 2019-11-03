3b3b64d9ab8bd0fd57fa5588958ff5900d56a63e
Huajian Mao <huajianmao@gmail.com>
2019-11-03 10:08:21 +0800

    Add solution for problem 62 unique paths

---
+++ solutions/a0062uniquepaths.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+################################################
+#
+# URL:
+# =====
+# https://leetcode.com/problems/unique-paths/
+#
+# DESC:
+# =====
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+#
+# The robot can only move either down or right at any point in time.
+# The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+#
+# How many possible unique paths are there?
+#
+#
+# Above is a 7 x 3 grid. How many possible unique paths are there?
+#
+# Note: m and n will be at most 100.
+#
+# Example 1:
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation:
+# From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# 1. Right -> Right -> Down
+# 2. Right -> Down -> Right
+# 3. Down -> Right -> Right
+#
+# Example 2:
+# Input: m = 7, n = 3
+# Output: 28
+#
+################################################
+
+
+class Solution:
+  def uniquePaths(self, m: int, n: int) -> int:
+    matrix = [[1 for _ in range(m)] for _ in range(n)]
+    for i in range(1, n):
+      for j in range(1, m):
+        matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1]
+    return matrix[-1][-1]
---
+++ tests/test_a0062uniquepaths.py
@@ -0,0 +1,21 @@
+from solutions.a0062uniquepaths import Solution
+
+solution = Solution()
+
+def test_uniquePaths1():
+  m, n = 3, 2
+  expect = 3
+  actual = solution.uniquePaths(m, n)
+  assert actual == expect
+
+def test_uniquePaths2():
+  m, n = 7, 3
+  expect = 28
+  actual = solution.uniquePaths(m, n)
+  assert actual == expect
+
+def test_uniquePaths3():
+  m, n = 23, 12
+  expect = 193536720
+  actual = solution.uniquePaths(m, n)
+  assert actual == expect
