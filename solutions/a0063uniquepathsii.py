d1d9a71f9562a11009712f821b7dfc660602e6e3
Huajian Mao <huajianmao@gmail.com>
2019-11-04 09:14:42 +0800

    Add solution for problem 63 unique paths ii

---
+++ solutions/a0063uniquepathsii.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+################################################
+#
+# URL:
+# =====
+# https://leetcode.com/problems/unique-paths-ii/
+#
+# DESC:
+# =====
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+#
+# The robot can only move either down or right at any point in time.
+# The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+#
+# Now consider if some obstacles are added to the grids. How many unique paths would there be?
+#
+#
+#
+# An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+#
+# Note: m and n will be at most 100.
+#
+# Example 1:
+# Input:
+# [
+#   [0,0,0],
+#   [0,1,0],
+#   [0,0,0]
+# ]
+# Output: 2
+# Explanation:
+# There is one obstacle in the middle of the 3x3 grid above.
+# There are two ways to reach the bottom-right corner:
+# 1. Right -> Right -> Down -> Down
+# 2. Down -> Down -> Right -> Right
+#
+################################################
+from typing import List
+
+
+class Solution:
+  def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    m = len(obstacleGrid)
+    if m == 0:
+      return 0
+    n = len(obstacleGrid[0])
+    if n == 0:
+      return 0
+    obstacleGrid[0][0] = 1 - obstacleGrid[0][0]
+
+    for i in range(1, m):
+      obstacleGrid[i][0] = obstacleGrid[i - 1][0] * (1 - obstacleGrid[i][0])  # set it to 0 if the previous one is 0
+    for i in range(1, n):
+      obstacleGrid[0][i] = obstacleGrid[0][i - 1] * (1 - obstacleGrid[0][i])
+
+    for i in range(1, m):
+      for j in range(1, n):
+        if obstacleGrid[i][j] == 1:
+          obstacleGrid[i][j] = 0
+        else:
+          obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+
+    return obstacleGrid[-1][-1]
---
+++ tests/test_a0063uniquepathsii.py
@@ -0,0 +1,64 @@
+from solutions.a0063uniquepathsii import Solution
+import json
+
+solution = Solution()
+
+
+def test_uniquePaths1():
+  obstacleGrid = json.loads("""
+    [
+      [0,0,0],
+      [0,1,0],
+      [0,0,0]
+    ]
+  """)
+  expect = 2
+  actual = solution.uniquePathsWithObstacles(obstacleGrid)
+  assert actual == expect
+
+
+def test_uniquePaths2():
+  obstacleGrid = json.loads("""
+    [
+      [0,0],
+      [0,1],
+      [0,0]
+    ]
+  """)
+  expect = 1
+  actual = solution.uniquePathsWithObstacles(obstacleGrid)
+  assert actual == expect
+
+
+def test_uniquePaths3():
+  obstacleGrid = json.loads("""
+    [
+      [1]
+    ]
+  """)
+  expect = 0
+  actual = solution.uniquePathsWithObstacles(obstacleGrid)
+  assert actual == expect
+
+
+def test_uniquePaths4():
+  obstacleGrid = json.loads("""
+    [
+      [1,0]
+    ]
+  """)
+  expect = 0
+  actual = solution.uniquePathsWithObstacles(obstacleGrid)
+  assert actual == expect
+
+
+def test_uniquePaths5():
+  obstacleGrid = json.loads("""
+    [
+      [1],
+      [0]
+    ]
+  """)
+  expect = 0
+  actual = solution.uniquePathsWithObstacles(obstacleGrid)
+  assert actual == expect
