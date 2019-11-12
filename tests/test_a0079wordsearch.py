28f43bbb308008b99b56e5f70ad695af0e0124e9
Huajian Mao <huajianmao@gmail.com>
2019-11-12 08:55:45 +0800

    Add solution for problem 79 word search

---
+++ tests/test_a0079wordsearch.py
@@ -0,0 +1,20 @@
+from solutions.a0079wordsearch import Solution
+from utils.listutil import ListUtil
+
+solution = Solution()
+
+
+def test_exist1():
+  board = [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
+  ]
+  assert solution.exist(board, "ABCCED")
+  assert solution.exist(board, "SEE")
+  assert not solution.exist(board, "ABCB")
+
+
+def test_exist2():
+  board = []
+  assert not solution.exist(board, "ABCCED")
